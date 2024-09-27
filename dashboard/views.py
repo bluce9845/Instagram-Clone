4fc0c2ab3384b15d96ce6817c73e41bb64a285eb
Rizki Fiananda <rizkyfiananda@gmail.com>
2024-09-27 09:19:12 +0700

    Add login session

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #authenticated
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging, Please Try Again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
