2fb4392aa118d98936e2dfbc7c1adacf41a98717
Rizki Fiananda <rizkyfiananda@gmail.com>
2024-10-02 08:24:59 +0700

    added Sign Up session

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path("", views.home, name="home"),
     path('sign-up/', views.register, name="register"),
-    path('', auth_views.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name='login'),
     path("logout/", views.logout_user, name="logout"),
 ]
