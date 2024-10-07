ef891a9d6d5ad159d717436023d6f2735fa3453e
Rizki Fiananda <rizkyfiananda@gmail.com>
2024-10-07 10:46:18 +0700

    added profile section

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from .import views
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView
+from authy.views import UserProfile
+
 
 urlpatterns = [
     path("", views.home, name="home"),
     path('sign-up/', views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name='login'),
     path("logout/", views.logout_user, name="logout"),
+
+    # profile
+    path("<username>/", UserProfile, name="profile"),
 ]
--- instagramClone/urls.py
+++ instagramClone/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from authy.views import UserProfile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include('authy.urls')),
     path('', include('dashboard.urls')),
 
-    # profile
-    path("<username>/", UserProfile, name="profile"),
+
 ]
